0e5435feae16c8d9014ef194838da17b2f9aadb5
Aziz <abdullaev12@bk.ru>
2024-02-02 16:32:25 +0300

    Смена языка

--- app/src/main/java/com/abdullaev/localerus/MainActivity.java
+++ app/src/main/java/com/abdullaev/localerus/MainActivity.java
@@ -5,23 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] strLocal = {"Русский язык", "Английский язык"};
     Button buttonLocal;
     Spinner spinnerLocal;
-
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         view();
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, strLocal);
+        spinnerLocal.setAdapter(adapter);
     buttonLocal.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -37,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
     public void view() {
         buttonLocal = findViewById(R.id.button);
         spinnerLocal = findViewById(R.id.spinner);
+        text = findViewById(R.id.textView);
     }
+
 
 
 }
